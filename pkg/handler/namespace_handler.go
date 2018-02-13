285e27c562d4ce2d6a29eb12b1ff98f792774102
huy le <huy.dinh.le.89@gmail.com>
2018-02-12 18:02:05 -0800

    added more code to namespace_handler

--- pkg/handler/namespace_handler.go
+++ pkg/handler/namespace_handler.go
@@ -13,6 +13,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	lister_v1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
@@ -22,7 +23,8 @@ import (
 
 type NamespaceHandler struct {
 	client     kubernetes.Interface
-	configmaps []api_v1.ConfigMap
+	configmaps *api_v1.ConfigMapList
+	namespaces *api_v1.NamespaceList
 }
 
 // NewNameSpaceHandler return a handler that help us to manage Namespace
@@ -39,7 +41,55 @@ func (nh *NamespaceHandler) ProcessNamespace() error {
 
 	// go through each and every configmaps , fetch out the yaml files one by one, then install them into the new namespace
 	// based on their types, right now we support configmaps and secrets
+
+	for k, v := range nh.namespaces.Items {
+		for k, v := range nh.configmaps.Items {
+		}
+	}
 }
 
-func getConfigMaps(labels []string, namespace string, client kubernetes.Interface) {
+func applyConfigMapToNameSpace(namespace string, client kubernetes.Interface, cm api_v1.ConfigMap) err {
+	for _, f := range cm.Data {
+
+		// parse the yaml into an object
+
+		// have a switch statement.
+		decode := scheme.Codecs.UniversalDeserializer().Decode
+		obj, groupVersionKind, err := decode([]byte(f), nil, nil)
+
+		if err != nil {
+			log.Fatal(fmt.Sprintf("Error while decoding YAML object. Err was: %s", err))
+		}
+
+		// now use switch over the type of the object
+		// and match each type-case
+		switch o := obj.(type) {
+		case *api_v1.ConfigMap:
+			cmClient := client.CoreV1().ConfigMaps(namespace)
+			_, err := cmClient.Create(o)
+			if err != nil {
+			}
+		case *api_v1.Secret:
+			cmClient := client.CoreV1().ConfigMaps(namespace)
+		default:
+			//o is unknown for us
+		}
+	}
+
+	return nil
+}
+
+// get all the configmaps that has the labels list
+func getConfigMaps(label string, namespace string, client kubernetes.Interface) (*api_v1.ConfigMapList, error) {
+
+	cmClient := client.CoreV1().ConfigMaps("kube-system")
+
+	cmList, err := cmClient.List(meta_v1.ListOptions{
+		LabelSelecto: label,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return cmList
 }
